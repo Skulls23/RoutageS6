466654cb3fbfc8ea83bcc1e001148d09d8a55c74
Dim145 <dimitri.bleach@gmail.com>
2021-02-24 10:35:57 +0100

    add other graph dependencies

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,12 @@
+import org.graphstream.graph.implementations.SingleGraph;
+import org.graphstream.ui.graphicGraph.GraphicNode;
+
 public class Main
 {
     public static void main(String[] args)
     {
         System.out.println("YO");
+
+        SingleGraph graph = new SingleGraph("Test");
     }
 }
