a82628000cffb001b34497cb09d5e519a9ec5718
Florian Boireau <florian23russie@hotmail.fr>
2021-02-24 12:52:39 +0100

    Affichage du passage le plus court, en mode textuel ou graphique

--- src/main/java/Routage/Main.java
+++ src/main/java/Routage/Main.java
@@ -1,6 +1,10 @@
 package Routage;
 
 import Routage.ihm.IHMGUI;
+import Routage.ihm.Metier;
+import org.graphstream.algorithm.Dijkstra;
+import org.graphstream.algorithm.Dijkstra.Element;
+import org.graphstream.graph.Graph;
 import org.graphstream.graph.implementations.SingleGraph;
 import org.graphstream.graph.*;
 
@@ -19,9 +23,23 @@ public class Main
         graph.addNode("A");
         graph.addNode("B");
         graph.addNode("C");
-        graph.addEdge("AB", "A", "B");
-        graph.addEdge("BC", "B", "C");
-        graph.addEdge("CA", "C", "A");
+        graph.addNode("D");
+        graph.addNode("E");
+        graph.addNode("F");
+        graph.addEdge("AB", "A", "B").setAttribute("length", 2);
+        graph.addEdge("BC", "B", "C").setAttribute("length", 3);
+        graph.addEdge("CF", "C", "F").setAttribute("length", 40);
+        graph.addEdge("AE", "A", "E").setAttribute("length", 7);
+        graph.addEdge("EF", "E", "F").setAttribute("length", 1);
+        graph.addEdge("EC", "E", "C").setAttribute("length", 3);
+        graph.addEdge("ED", "E", "D").setAttribute("length", 1);
+
+        Dijkstra dijkstra = new Dijkstra(Element.EDGE, null, "length");
+        dijkstra.init(graph);
+        dijkstra.setSource(graph.getNode("A"));
+        dijkstra.compute();
+
+        Metier.getPlusCourtCheminGraphique(graph, "A", "F");
 
         this.ihm = new IHMGUI(this, graph);
     }
