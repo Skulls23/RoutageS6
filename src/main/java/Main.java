9fef3233e2ad46fc0de9b2997e025259705f9dd6
Dim145 <dimitri.bleach@gmail.com>
2021-02-24 10:53:05 +0100

    uygfjehaf

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,5 +8,15 @@ public class Main
         System.out.println("YO");
 
         SingleGraph graph = new SingleGraph("Test");
+
+        graph.addNode("N1");
+        graph.addNode("N2");
+        graph.addNode("N3");
+
+        graph.addEdge("N1N2", "N1", "N2", true);
+        graph.addEdge("N1N3", "N1", "N3", true);
+        graph.addEdge("N2N3", "N2", "N3", true);
+
+        graph.display();
     }
 }
