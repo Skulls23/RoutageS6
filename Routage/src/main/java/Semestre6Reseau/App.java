23df6a836b52ffd522578f1d13147646a33402d1
Florian Boireau <florian23russie@hotmail.fr>
2021-02-24 10:16:49 +0100

    Probleme de merge

---
+++ Routage/src/main/java/Semestre6Reseau/App.java
@@ -0,0 +1,13 @@
+package Semestre6Reseau;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Routage/src/test/java/Semestre6Reseau/AppTest.java
@@ -0,0 +1,20 @@
+package Semestre6Reseau;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
