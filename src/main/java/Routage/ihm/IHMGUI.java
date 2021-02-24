0356702b68a57c1d1a702282bdd92a0413715054
Dim145 <dimitri.bleach@gmail.com>
2021-02-24 12:22:11 +0100

    ajouts boutons

--- src/main/java/Routage/ihm/IHMGUI.java
+++ src/main/java/Routage/ihm/IHMGUI.java
@@ -9,8 +9,11 @@ import java.awt.*;
 
 public class IHMGUI extends JFrame
 {
-    private SingleGraph theGraph;
-    private Main ctrl;
+    private final SingleGraph theGraph;
+    private final Main ctrl;
+
+    private JButton ajoutPC;
+    private JButton ajoutRouteur;
 
     public IHMGUI( Main ctrl, SingleGraph graph )
     {
@@ -18,6 +21,28 @@ public class IHMGUI extends JFrame
         this.theGraph = graph;
 
         this.add(new PanelGraphViewer(graph), BorderLayout.CENTER);
+
+        this.ajoutPC = new JButton("Ajouter PC");
+        this.ajoutPC.addActionListener(event ->
+        {
+            int num = this.theGraph.getNodeCount();
+            this.theGraph.addNode("PC" + num);
+            this.theGraph.getNode("PC" + num).setAttribute("label", "PC" + num);
+        });
+
+        this.ajoutRouteur = new JButton("Ajouter Routeur");
+        this.ajoutRouteur.addActionListener(event ->
+        {
+            int num = this.theGraph.getNodeCount();
+            this.theGraph.addNode("RO" + num);
+            this.theGraph.getNode("RO" + num).setAttribute("label", "RO" + num);
+        });
+
+        JPanel panelTMP = new JPanel();
+        panelTMP.add(this.ajoutPC);
+        panelTMP.add(this.ajoutRouteur);
+
+        this.add(panelTMP, BorderLayout.EAST);
 
         this.pack();
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
