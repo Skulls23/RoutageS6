6a81a8ebd4127e6cc0aa1ed1bff1271cd6eb3a17
Florian Boireau <florian23russie@hotmail.fr>
2021-03-18 18:45:18 +0100

    ajout de details dans la Javadoc de Metier.java

--- src/main/java/Routage/metier/Metier.java
+++ src/main/java/Routage/metier/Metier.java
@@ -1,11 +1,8 @@
 package Routage.metier;
 
-import Routage.Main;
 import org.graphstream.algorithm.Dijkstra;
 import org.graphstream.graph.Edge;
-import org.graphstream.graph.Graph;
 import org.graphstream.graph.Node;
-import org.graphstream.graph.Path;
 import org.graphstream.graph.implementations.SingleGraph;
 
 import java.util.*;
@@ -17,25 +14,38 @@ https://javadoc.io/doc/org.graphstream
  */
 public class Metier
 {
-    private final Main ctrl;
     private final SingleGraph graph;
-
     private final ArrayList<Node[]> listchemin;
 
-    public Metier(Main ctrl, SingleGraph graph)
+
+    /**
+     * Constructeur
+     * @param graph Le graph
+     */
+    public Metier(SingleGraph graph)
     {
-        this.ctrl = ctrl;
         this.graph = graph;
 
         this.listchemin = new ArrayList<>();
     }
 
+    /**
+     * Calcule le plus court chemin entre pointDebut et pointFin
+     * @param pointDebut Le Node de départ
+     * @param pointFin Le Node d'arrivé
+     * @return String du chemin le plus court
+     */
     public String getPlusCourtCheminTextuel(String pointDebut, String pointFin)
     {
         Dijkstra dijkstra = this.setupDijkstra(pointDebut);
         return dijkstra.getPath(graph.getNode(pointFin)) + " " + dijkstra.getPathLength(graph.getNode(pointFin));
     }
 
+    /**
+     * Colorie en vert les Nodes qui ont servi de point de passage entre pointDebut et pointFin
+     * @param pointDebut Le Node de départ
+     * @param pointFin Le Node d'arrivé
+     */
     public void getPlusCourtCheminGraphique(String pointDebut, String pointFin)
     {
         this.reinitialiserCouleurs();
@@ -49,6 +59,9 @@ public class Metier
             edge.setAttribute("ui.style", "fill-color: green;");
     }
 
+    /**
+     * Remet les couleurs des noeuds en noir
+     */
     public void reinitialiserCouleurs()
     {
         for (int i = 0; i < this.graph.getNodeCount(); i++)
@@ -58,12 +71,23 @@ public class Metier
             this.graph.getEdge(i).setAttribute("ui.style", "fill-color: black;");
     }
 
+    /**
+     * Execute getPlusCourtCheminTextuel suivi de getPlusCourtCheminGraphique
+     * @param pointDebut Le Node de départ
+     * @param pointFin Le Node d'arrivé
+     * @return String resultant de getPlusCourtCheminTextuel
+     */
     public String getPlusCourtCheminTextuelEtGraphique(String pointDebut, String pointFin)
     {
         this.getPlusCourtCheminGraphique(pointDebut, pointFin);
         return this.getPlusCourtCheminTextuel(pointDebut, pointFin);
     }
 
+    /**
+     * Affiche la table de routage résultant du graph
+     * @return une TreeMap qui lie un nom de routeur de depart a un nom de routeur d'arrivé
+     * qui est lui meme lié a tout ses voisins et leurs poids
+     */
     public TreeMap<String, TreeMap<String, TreeMap<String, Double>>> getTableRoutage()
     {
         SingleGraph graphTemp = getCopieGraph();
@@ -73,9 +97,9 @@ public class Metier
 
         TreeMap<String, TreeMap<String, TreeMap<String, Double>>> hashSite = new TreeMap<>();
 
+        //Parcours de tout les Nodes du graph
         graphTemp.nodes().forEachOrdered(pointDebut ->
         {
-
             if( pointDebut.getId().contains("PC") )
                 return;
 
@@ -87,7 +111,8 @@ public class Metier
             Object[] tabVoisin          = pointDebut.neighborNodes().toArray();
             Object[] tabEdgeVoisin      = pointDebut.edges().toArray();
             double[] tabPoidsVersVoisin = new double[tabVoisin.length];
-            //on definit ses voisins et on les enleve pour les remettre plus tard
+
+            //on definit les Nodes voisins
             for (int k = 0; k < tabVoisin.length; k++)
             {
                 Edge e = pointDebut.getEdge(k);
@@ -95,11 +120,12 @@ public class Metier
                 tabPoidsVersVoisin[k] = e.getAttribute("length") == null ? Double.NEGATIVE_INFINITY : (double) e.getAttribute("length");
             }
 
+            //on enleve les liaisons avec les voisins pour eviter qu'un paquet repasse 2x par le meme Node
             for (Object o : tabEdgeVoisin) graphTemp.removeEdge((Edge) o);
 
             TreeMap<String, TreeMap<String, Double>> listAllDest = new TreeMap<>();
 
-            //on trouve le noeud de fin
+            //Parcours des Nodes d'arrivé
             graphTemp.nodes().forEachOrdered( pointFin ->
             {
                 if( pointDebut == pointFin || pointFin.getId().contains("PC") )
@@ -110,6 +136,7 @@ public class Metier
                 this.setupDijkstra(pointDebut.getId());
 
                 HashMap<String, Double> mapPoidsVoisinVersFinal = new HashMap<>(); //lie un poids de chemin a un nom de noeud
+
                 //on parcours du premier voisin du point de depart vers le point de fin, puis on prend le deuxieme voisin, ...
                 for (int k = 0; k < tabVoisin.length; k++)
                 {
@@ -123,7 +150,7 @@ public class Metier
                 }
                 TreeMap<String, Double> treeMap = new TreeMap<>((o1, o2) -> 1);// si on met autre chose que treeMap, certaines valeur manquent...
                 // si on change le comparateur pour comparer directement les Double, pareil.
-                // seule solution trouver, trier "manuellement" et mettre un comparateur inutile.
+                // seule solution trouver, trier "manuellement" et mettre un comparateur "inutile".
 
                 Double[] tab = mapPoidsVoisinVersFinal.values().toArray(new Double[0]);
                 Arrays.sort(tab);
@@ -131,6 +158,7 @@ public class Metier
                 ArrayList<String> set = new ArrayList<>(mapPoidsVoisinVersFinal.keySet());
                 ArrayList<Double> col = new ArrayList<>(mapPoidsVoisinVersFinal.values());
 
+                //liaison des couts en fonctions des chemins
                 for (Double aDouble : tab)
                 {
                     int i = col.indexOf(aDouble);
@@ -146,6 +174,8 @@ public class Metier
             });
 
             hashSite.put(pointDebut.getId(), listAllDest);
+
+            //Remise de toutes les liaisons avec les Nodes voisins pour reboucler proprement
             for (Object o : tabEdgeVoisin)
             {
                 Edge e = ((Edge) o);
@@ -160,6 +190,11 @@ public class Metier
         return hashSite;
     }
 
+    /**
+     * Permet de definir le point de depart de l'algorithme de Dijkstra
+     * @param pointDebut Le Node de départ
+     * @return l'objet qui pourra executer l'algorithme de Dijkstra
+     */
     private Dijkstra setupDijkstra(String pointDebut)
     {
         Dijkstra dijkstra = new Dijkstra(Dijkstra.Element.EDGE, null, "length");
@@ -188,6 +223,12 @@ public class Metier
         return graphTemp;
     }
 
+    /**
+     * Permet d'avoir tout les nodes qui ont servi de points de passsages entre pointDebut et pointFin
+     * @param pointDebut Le Node de départ
+     * @param pointFin Le Node d'arrivé
+     * @return Un Iterable de Node
+     */
     private Iterable<Node> getCheminParNode(String pointDebut, String pointFin)
     {
         Dijkstra dijkstra = setupDijkstra(pointDebut);
